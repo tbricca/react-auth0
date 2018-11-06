6fed6c741efcfecb40e2f537c02fa2cefa6f8a56
tbricca <tbricca@gmail.com>
2018-11-06 10:31:00 -0800

    created a function to track all of the changes to the application

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// global variable
+let state = {}
+// this function will track all of the changes in the state of the application 
+// anytime we want to change anything we'll want to use this function to do the changes
+// & to also re-render the application 
+window.setState = (changes) => {
+    // using object.assign so we don't mutuate the actual state
+    state = Object.assign({}, state, changes);
+
+    ReactDOM.render(<App />, document.getElementById('root'));
+};
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
